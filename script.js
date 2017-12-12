086e0845664f5826b81f83d41786c30644a57a32
Kathy Lambert <Kathy.Lambert@LotusDesignsAgency.com>
2017-12-12 16:08:36 -0500

    Nested the Div to text function within the EventList

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 var flashCardQA = [
   {
-    question1: "What is JavaScript?",
-    answer1:
+    question: "What is JavaScript?",
+    answer:
       "JavaScript (JS) is a programming language mostly used client-side to dynamically script webpages, but often also server -side, using packages such as Node.js."
   },
   {
-    question2: "What is a Node?",
-    answer2:
+    question: "What is a Node?",
+    answer:
       "Node is an interface from which a number of DOM API object types inherit; it allows these various types to be treated similarly, for example inheriting the same set of methods, or being tested in the same way."
   },
   {
-    question3:
+    question:
       "What is a function declaration compared to a function expression?",
-    answer3:
+    answer:
       '"declaration: function myFunc(){ ... } expression: var myFunc = function() { ...}"'
   }
 ];
@@ -22,32 +22,35 @@ var backCard = document.getElementsByClassName("backCard")[0]
  
 // Reference: https://stackoverflow.com/questions/19197140/javascript-append-text-into-div
 
-frontCard.innerHTML = flashCardQA[0].question1;
+frontCard.innerHTML = flashCardQA[0].question;
 
-frontCard.innerHTML += flashCardQA[1].question2;
+// frontCard.innerHTML += flashCardQA[1].question2;
 
-frontCard.innerHTML += flashCardQA[2].question3;
+// frontCard.innerHTML += flashCardQA[2].question3;
 
-backCard.innerHTML += flashCardQA[0].answer1;
+// backCard.innerHTML += flashCardQA[0].answer1;
 
 let randomIndex = Math.floor(Math.random()*flashCardQA.length)
     //Invokes random question generator
 
+let index = 0
+
 document.addEventListener('keydown', function(ev) { 
     if (ev.key === 'ArrowUp') {
         console.log(ev);
+        frontCard.setAttribute('hidden', true);
+        backCard.removeAttribute("hidden");
+        index++
+        frontCard.innerHTML = flashCardQA[index].question;
+        //if add another action =, add if statement
+    }
+    if (ev.key === 'ArrowDown'){
+        console.log(ev);
+        backCard.setAttribute("hidden", true);
+        frontCard.removeAttribute("hidden");
         //if add another action =, add if statement
     }
     //Invoke EventListener for keyUP
         //Create a function for the flipCard motion
     
 })
-
-document.addEventListener('keydown', function(ev) { 
-    if (ev.key === 'ArrowDown'){
-        console.log(ev);
-        //if add another action =, add if statement
-    }})
-    //Invoke EventListener for keyDown
-        //Create a function for the flipCard motion
-        
